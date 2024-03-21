52aa7f369ffb10443c97d374046eaf041f92fd5f
Vaibhav Garg <113363806+vaibhav25garg@users.noreply.github.com>
2024-03-21 17:47:27 +0530

    Update App.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,20 +1,26 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import { ReactDOM } from "react-dom";
 import HomePage from "./HomePage";
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-  }
+ReactDOM.createRoot(document.getElementById('app')).render(
+  <React.StrictMode>
+    <HomePage />
+  </React.StrictMode>
+)
+// export default class App extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
 
-  render() {
-    return (
-      <div className="center">
-        <HomePage />
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div className="center">
+//         <HomePage />
+//       </div>
+//     );
+//   }
+// }
 
-const appDiv = document.getElementById("app");
-render(<App />, appDiv);
+// const appDiv = document.getElementById("app");
+// render(<App />, appDiv);
