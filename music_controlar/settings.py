c86dee2fb974ab6371c5bb4aff29a639db03ee14
Vaibhav Garg <113363806+vaibhav25garg@users.noreply.github.com>
2024-03-21 22:49:44 +0530

    new update settings.py

--- music_controlar/settings.py
+++ music_controlar/settings.py
@@ -14,6 +14,13 @@ import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -65,29 +72,34 @@ TEMPLATES = [
     },
 ]
 
-DATABASES['default'] = dj_database_url.parse("postgres://spotify_song_room_user:chFK6vKy0ly9P7i3pTsZxgpboafKJdzf@dpg-cnstdiol5elc73fmmcl0-a.oregon-postgres.render.com/spotify_song_room")
+WSGI_APPLICATION = 'music_controlar.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+database_url = os.environment.get("DATABASE_URL")
+DATABASES['default'] = dj_database_url.parse(database_url)
+# 
+
+# import os
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'spotify',
+#         'USER': 'postgres',
+#         'PASSWORD': 'vg2568@#',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
 #     }
 # }
 
-import os
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'spotify',
-        'USER': 'postgres',
-        'PASSWORD': 'vg2568@#',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -131,4 +143,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
