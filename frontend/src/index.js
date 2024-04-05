516d03748a33aa2e7d24464b0c98806eb56435f1
Vaibhav <vaibhavgarg977@gmail.com>
2024-04-05 14:16:44 +0530

    new com frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,18 @@
-// import App from "./components/App.js";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+// import App from './components/HomePage';
+import reportWebVitals from './reportWebVitals';
+import HomePage from './components/HomePage';
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(
+  <React.StrictMode>
+    <HomePage />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
